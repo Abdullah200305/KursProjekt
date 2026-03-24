0e11856c3964d784ccaec219a1a27cd2e7fde07c
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2026-03-24 12:22:09 +0100

    done

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     // terminal output
     printf("%d\n", ab);
     fflush(stdout);
-
+        // done
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
