d55cbfa6fe74ae3b24a4a1747de0a222c50d4daf
Abdullah200305 <abodyalwan23@gmail.com>
2026-03-30 10:57:40 +0200

    initial

--- src/main.c
+++ src/main.c
@@ -9,8 +9,10 @@ int main(int argc, char *argv[])
     fflush(stdout);
         // done
 
+    
+    
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    SDL_Window *window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
