ff9e61390bc89f15234af24c00034783a35b1881
h6jt5r2s8g-crypto <h6jt5r2s8g@privaterelay.appleid.com>
2026-04-18 23:16:17 +0200

    har lagt structar till spelar, map och bomb i protocolet

--- shared/Include/network_protocol.h
+++ shared/Include/network_protocol.h
@@ -1,9 +1,12 @@
 #ifndef NETWORK_PROTOCOL_H
 #define NETWORK_PROTOCOL_H
+#define MAX_PLAYERS 4
+
 
 typedef enum {
     PACKET_JOIN_REQUEST = 1,
     PACKET_JOIN_ACCEPT = 2
+    PACKET_GAME_INIT = 3
 } PacketType;
 
 typedef struct {
@@ -14,5 +17,37 @@ typedef struct {
     int type;
     int clientId;
 } JoinAcceptPacket;
+
+typedef struct{
+    float x;
+    float y;
+    int active
+    int exploding;
+    int bombCarrier;
+    int timer;
+} BombNetData;
+
+typedef struct{
+    float x;
+    float y;
+    int lives;
+    int alive;
+
+} PlayerNetData
+
+typedef struct {
+    int width;
+    int height;
+} MapNetData;
+
+typedef struct {
+    int type;
+    int numPlayers;
+    int clientId;
+    MapNetData map;
+    PlayerNetData players[MAX_PLAYERS];
+    BombNetData bomb;
+} GameInitPacket;
+
 
 #endif
