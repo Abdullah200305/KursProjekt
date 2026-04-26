0b4496e2388c172432a628e830bc8373fe2f07f4
Melvin <melvinfk05@gmail.com>
2026-04-27 00:50:40 +0200

    '_'

--- include/Player.h
+++ include/Player.h
@@ -24,12 +24,12 @@ float getPlayerVelocityY(Player player);
 float getPlayerWidth(Player player);
 float getPlayerHeight(Player player);
 int getPlayerLives(Player player);
-float getPlayerSpeedTimer(Player player);
 int getPlayerAnimationFrame(Player player);
-
-
+float getPlayerSpeedTimer(Player player);
 float getPlayerFreezeTimer(Player player);
 float getPlayerSizeUpTimer(Player player);
+int getPlayerShield(Player player);
+float getPlayerShieldTimer(Player player);
 
 //Setters metoder
 void setPlayerX(Player player, float x);
@@ -37,14 +37,14 @@ void setPlayerY(Player player, float y);
 void setPlayerVelocity(Player player, float vx, float vy);
 void setPlayerPosition(Player player, float x, float y);
 void setPlayerSize(Player player, float width, float height);
+void setPlayerAnimation(Player player);
 void setPlayerSpeedYX(Player player, float speedY, float speedX);
 void setPlayerSpeedTimer(Player player, float timer);
-void setPlayerAnimation(Player player);
-
-
-//Player Movement
 void setPlayerFreezeTimer(Player player, float timer);
 void setPlayerSizeUpTimer(Player player, float timer);
+void setPlayerShield(Player player, int value);
+void setPlayerShieldTimer(Player player, float timer);
+
 //MISC FUNCTIONS
 
 void playerMovement(
