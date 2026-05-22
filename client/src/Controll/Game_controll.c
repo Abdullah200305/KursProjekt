0a9ee2ad0ea711e207d736a8d7169cece684a5a6
Abdullah200305 <112935137+Abdullah200305@users.noreply.github.com>
2026-05-22 11:38:53 +0200

    del

--- client/src/Controll/Game_controll.c
+++ client/src/Controll/Game_controll.c
@@ -183,7 +183,7 @@ void game_update(Game *game, Renderer *renderer)
     Background_Image_Render(renderer);
 
     // this for test
-    Render_Map(renderer, game->map);
+   // Render_Map(renderer, game->map);
 
     AbilitySystem_render(game->abilitySystem, renderer);
     for (int i = 0; i < game->numPlayers; i++)
